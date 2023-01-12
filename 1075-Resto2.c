ff966e3e2267ecebff952c3598b4d878955014c7
kelvson <kelvsonnilson01@gmail.com>
2023-01-12 15:26:21 -0300

    Beecrowd Exercises.

--- 1075-Resto2.c
+++ 1075-Resto2.c
@@ -4,5 +4,7 @@ int main(){
     int n;
     scanf("%d", &n);
 
-    for(int i=1; i<1000; i++) printf((i % n == 2) ? "%d\n" : "", i);
+    for(int i=1; i<10000; i++) printf((i % n == 2) ? "%d\n" : "", i);
+
+    return 0;
 }
